9a3cd7b124a9293d84ea3335c053d19996d6d03c
paragsarvoday <117572668+paragsarvoday@users.noreply.github.com>
2025-03-13 08:00:21 +0000

    Deploying to gh-pages from @ paragsarvoday/paragsarvoday.github.io@3085d5dbbd4bc40af70c2488d3af66a7036eb064 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -490,14 +490,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%70%61%72%61%67.%73%61%68%75@%69%69%74%67%6E.%61%63.%69%6E", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/paragsarvoday", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/paragsarvoday", "_blank");
         },
       },{
         id: 'social-rss',
@@ -507,18 +514,18 @@ ninja.data = [{
           window.open("/feed.xml", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
+        id: 'social-x',
+        title: 'X',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://twitter.com/paragsarvoday", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-youtube',
+        title: 'YouTube',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://youtube.com/@paragsarvoday7706", "_blank");
         },
       },{
       id: 'light-theme',
