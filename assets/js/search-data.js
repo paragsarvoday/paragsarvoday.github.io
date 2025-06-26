64373fc1e411205d9b11e280348ed189503f6300
paragsarvoday <117572668+paragsarvoday@users.noreply.github.com>
2025-06-26 18:58:49 +0000

    Deploying to gh-pages from @ paragsarvoday/paragsarvoday.github.io@56708b63c772a5308c40abdfa58450a89bb06704 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,7 +23,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
-        },{id: "post-image-pyramids-and-corner-detection",
+        },{id: "post-a-cool-image-encryption-technique",
+      
+        title: "A cool image encryption technique",
+      
+      description: "putting bit-planes to good use",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/distill/";
+        
+      },
+    },{id: "post-image-pyramids-and-corner-detection",
       
         title: "Image Pyramids and Corner Detection",
       
